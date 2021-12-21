75d2e7bd6e49562ab986e8279b507b2b6e60edd8
eld120 <devteam@origma.io>
2021-12-21 17:32:06 -0600

    pylists now works as intended, I now understand regex lookback groups a bit better

--- python/pylists.py
+++ python/pylists.py
@@ -2,36 +2,55 @@ import re
 
 
 if __name__ == "__main__":
-    N = int(input())
+    N = int(input("enter:"))
     
     count = 0
     lst = []
     
     while count < N:
+        count += 1
         ans = input()
         
         if 'insert' in ans:
-            end = re.search(r'(?<=insert\s\d)\d+', ans)
-            middle = re.search(r'(?<=insert\s)\d+', ans)
-            lst.insert(int(middle.group(0)), int(end.group(0)))    
-        if 'print' in ans:
+            try:
+                end = re.search(r'(?<=insert\s\d\s).*', ans)
+                middle = re.search(r'(?<=insert\s)\d+', ans)
+                lst.insert(int(middle.group(0)), int(end.group(0)))  
+                
+            except AttributeError:
+                print(end)
+                print('insert not found')
+                continue
+              
+        elif 'print' in ans:
             print(lst)
-        if 'remove' in ans:
+        elif 'remove' in ans:
+            try:
+                end = re.search(r'(?<=remove\s).*', ans)
+                lst.remove(int(end.group(0)))
+            except AttributeError:
+                print('remove not performed')
+                continue
             
-            end = re.search(r'(?<=insert\s)\d', ans)
-            lst.remove(int(end.group(0)))
-        if 'append' in ans:
-            end = re.search(r'(?<=insert\s)\d', ans)
-            lst.append(int(end.group(0)))
-        if 'sort' in ans:
+        elif 'append' in ans:
+            try:
+                end = re.search(r'(?<=append\s).*', ans)
+                lst.append(int(end.group(0)))
+            except AttributeError:
+                print('append not performed')
+                continue
+        elif 'sort' in ans:
             lst.sort()
-        if 'pop' in ans:
-            lst.pop()
-        if 'reverse' in ans:
+        elif 'pop' in ans:
+            try:
+                lst.pop()
+            except:
+                print('pop not peformed')
+        elif 'reverse' in ans:
             lst.reverse()
             
-            
-    print(lst)
+        
+    
         
         
        
