121143805f22fd5b2f8b48242bb5c58edbcbc939
eld120 <devteam@origma.io>
2021-12-22 11:58:21 -0600

    got the correct list comp solution but with some serious help, still not great at visualizing how to solve most of these problems

--- python/list_comps.py
+++ python/list_comps.py
@@ -9,11 +9,11 @@ if __name__ == '__main__':
     n = int(input())
     
     
-    def make_list(x: int, y: int, z: int) -> list:
-        return [[x, y, z] for x, y, z in [n, n , n]]
+    # def make_list(x: int, y: int, z: int) -> list:
+    #     return [[x, y, z] for x, y, z in [n, n , n]]
         
         
-    print([num1 for ])
+    print([[a, b, c] for a in range(0, x+1) for b in range(0, y+1) for c in range(0, z+1) if a+b+c != n])
     
     
     
