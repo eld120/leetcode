6a7f7d1c855cce548c3e6b9436602d7074f6cb0a
eld120 <devteam@origma.io>
2022-01-10 23:17:43 -0600

    again messy and not optimized but works, 2nd try to work out the bugs

---
+++ python/string_validators.py
@@ -0,0 +1,40 @@
+
+
+
+if __name__ == '__main__':
+    s = input()
+    alphanum = False
+    for i in range(0, len(s) +1):
+        if s[0+i:i+1].isalnum():
+            alphanum =True
+            break
+    print(alphanum)  
+    
+    alpha = False
+    for i in range(0, len(s) +1):
+        if s[0+i:i+1].isalpha():
+            alpha = True
+            break
+    print(alpha)
+    
+    digits = False
+    for i in range(0, len(s) +1):
+        if s[0+i:i+1].isdigit():
+            digits = True
+            break
+    print(digits)
+    
+    lowercase = False
+    for i in range(0, len(s) +1):
+        if s[0+i:i+1].islower():
+            lowercase =True
+            break
+    print(lowercase)
+   
+    uppercase = False
+    for i in range(0, len(s) +1):
+        if s[0+i:i+1].isupper():
+            uppercase = True
+            break
+    print(uppercase)
+    
