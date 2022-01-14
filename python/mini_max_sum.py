b3282ff7631a59892926a221727abb68bc51e869
eld120 <devteam@origma.io>
2022-01-14 15:35:26 -0600

    stumbled a bit here but found an easy enough solution

---
+++ python/mini_max_sum.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+def mini_max_sum(arr):
+    arr.sort()
+    arr2 = arr[1:]
+    arr3 = arr[:4]
+    print(f'{sum(arr3)} {sum(arr2)}')
+
+
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().rstrip().split()))
+    
+    mini_max_sum(arr)
