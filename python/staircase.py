e45751661bd4ffffc697bb9915e8e689bbc1c906
eld120 <devteam@origma.io>
2022-01-14 15:21:17 -0600

    quite easy

---
+++ python/staircase.py
@@ -0,0 +1,19 @@
+
+
+
+
+def staricase(n:int):
+    counter = 1
+    while n > 0:
+        
+        print(" " * (n-1) + '#' * counter)
+        counter += 1
+        n -= 1
+        
+
+
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
