6b28ad7d0558212e175c5303d8833ad67820dd36
[eld120] <devteam@origma.io>
2022-05-08 23:56:57 -0500

    loading the project correctly gets it to work - now turning this into a looping program with a graceful exit

--- csharp/csharp/fizzBuzz.cs
+++ csharp/csharp/fizzBuzz.cs
@@ -3,33 +3,42 @@
  * any number divisible by three is replaced by the word fizz and any number divisible by 
  * five by the word buzz. Numbers divisible by 15 become fizz buzz
  */
-Console.WriteLine("fizz buzz please enter a number");
+bool program = true;
+while (program == true)
+{
+    
+    Console.WriteLine("please enter a number or type quit");
 
-int answer;
-// Parse string to int - hopefully TryParse has some exception handling
-//try
-///{
+    int answer;
+    // Parse string to int - hopefully TryParse has some exception handling
+    //try
+    ///{
     var userAnswer = int.TryParse(Console.ReadLine(), out answer);
-//}
-// Catch null exception
-//catch (NullReferenceException)
-//{
-    Console.WriteLine("Please enter a number");
-//}
-// fizz buzz logic goes here
-if (answer % 15 == 0)
-{
-    Console.WriteLine("fizz buzz");
+    //}
+    // Catch null exception
+    //catch (NullReferenceException)
+    //{
+    //Console.WriteLine("Please enter a number");
+    //}
+    // fizz buzz logic goes here
+    if (userAnswer == false)
+    {
+        program == false;
+    }
+    if (answer % 15 == 0)
+    {
+        Console.WriteLine("fizz buzz");
+    }
+    else if (answer % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else if (answer % 3 == 0)
+    {
+        Console.WriteLine("fizz");
+    }
+    else
+    {
+        Console.WriteLine(answer);
+    }
 }
-else if (answer % 5 == 0)
-{
-    Console.WriteLine("buzz");
-}
-else if (answer % 3 == 0)
-{
-    Console.WriteLine("fizz");
-}else
-{
-    Console.WriteLine(answer);
-}
-Console.ReadLine();
