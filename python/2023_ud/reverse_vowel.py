aa07ebeaf210df9c9f88a7e6c95166a6a97516a5
Seyamack Afzali <seyamack@gmail.com>
2023-07-19 21:15:17 -0500

    works well enough, took a while getting here though

---
+++ python/2023_ud/reverse_vowel.py
@@ -0,0 +1,24 @@
+
+def reverseVowels(s: str) -> str:
+    # string -> (vowel, index) -> string
+    vowels = 'aeiouAEIOU'
+    
+    vowel_set = [letter for letter in s if letter in vowels]
+    
+    answer = ''
+    decrement = -1
+    for letter in s:
+        
+        if letter in vowels:
+            answer += vowel_set[decrement]
+            decrement -= 1
+        else:
+            answer += letter
+    return answer 
+    # print(answer)
+
+if __name__ == '__main__':
+    reverseVowels('sweatshop')
+
+
+    
