756f52e0baea185533b0245e76d7d6953f671f03
Seyamack Afzali <seyamack@gmail.com>
2024-01-05 22:33:04 -0600

    small fix avoiding potential edge case

--- ud_wordplay/words_ending_in_ghtly.py
+++ ud_wordplay/words_ending_in_ghtly.py
@@ -4,12 +4,16 @@ target = 'GHTLY'
 if __name__ == '__main__':
     with open('sowpods.txt', 'r') as f:
         #arr = []
-        print([val.strip('\n') for val in f.readlines() if target in val])
+        # print([val.strip('\n') for val in f.readlines() if target in val])
         # for val in f.readlines():
         #     if target in val:
         #         arr.append(val.rstrip('\n'))
                 
         # print(arr)
+
+        # the above code is flaky as the target could be anywhere in the data
+
+        print([val.strip('\n') for val in f.readlines() if target in val.strip('\n')[-5:]])
         
         
                     
