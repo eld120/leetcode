80007d675e28b84bc37d647a007ec29332b1fa95
Seyamack Afzali <seyamack@gmail.com>
2024-02-19 12:18:54 -0600

    need +1, edge case if findable is in last index

--- last_algo_course/binary_search.py
+++ last_algo_course/binary_search.py
@@ -5,23 +5,27 @@ def binary_find_stuff(searchable: List, hopefully_findable: int) -> int:
 
     high = len(searchable)
     low = 0
-    while low < high:
+    # if hopefully_findable > high or hopefully_findable < low:
+    #     raise ValueError
+    while low <= high:
         midpoint = (high + low)//2
-        #breakpoint()
         if hopefully_findable == midpoint:
-            
             return midpoint
         elif hopefully_findable < midpoint :
             high = midpoint
         else:
-            low = midpoint
+            low = midpoint + 1
         
 
 def test_one():
     assert binary_find_stuff([num for num in range(1,101)], 23) == 23
+    assert binary_find_stuff([1,2,3], 2) == 2 
 
 def test_two():
     assert binary_find_stuff([num for num in range(1,101)], 22) == 22
+
+def test_odd_number():
+    assert binary_find_stuff([1,2,3,4,5,6,7], 7) == 7
 
 
 def test_three():
