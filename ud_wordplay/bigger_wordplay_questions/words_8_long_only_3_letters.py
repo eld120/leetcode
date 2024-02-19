3b39175748189833d3d6be22a1832c75b73c5387
Seyamack Afzali <seyamack@gmail.com>
2024-02-18 21:04:43 -0600

    black reformatting

--- ud_wordplay/bigger_wordplay_questions/words_8_long_only_3_letters.py
+++ ud_wordplay/bigger_wordplay_questions/words_8_long_only_3_letters.py
@@ -1,33 +1,22 @@
-
-
 def words_fit_specific_criteria(file_path):
-    '''
+    """
     What are all of the words that are at least 8 letters long and use 3 or fewer different letters? For example, “REFERRER” is an answer to this question, because it uses only 3 different letters: R, E, and F.
-    '''
+    """
 
-    with open(file_path, 'r') as file:
-        
+    with open(file_path, "r") as file:
         list_of_words_only_three_letters = []
         for line in file:
-            word = line.rstrip('\n')
-            
-            
+            word = line.rstrip("\n")
+
             if len(word) > 7 and len(set(word)) <= 3:
                 list_of_words_only_three_letters.append(word)
 
-    return ' - '.join(list_of_words_only_three_letters)
+    return " - ".join(list_of_words_only_three_letters)
 
-
-
-
-
-
-    
 
 def test_two():
-    assert words_fit_specific_criteria('../sowpods_test.txt') == 'REFERRER'
+    assert words_fit_specific_criteria("../sowpods_test.txt") == "REFERRER"
 
 
-if __name__ == '__main__':
-    print(words_fit_specific_criteria('../sowpods.txt'))
-   
+if __name__ == "__main__":
+    print(words_fit_specific_criteria("../sowpods.txt"))
