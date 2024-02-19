7f4a845974e90c8a83c94c430729267fe7e32285
Seyamack Afzali <seyamack@gmail.com>
2024-02-18 21:15:11 -0600

    refactored with lists instead of strings, added a couple tests

--- ud_wordplay/bigger_wordplay_questions/words_with_three_double_letters.py
+++ ud_wordplay/bigger_wordplay_questions/words_with_three_double_letters.py
@@ -5,25 +5,28 @@ def find_lotsa_double_letters():
     What are all of the words that have at least 3 different double letters? For example, “BOOKKEEPER” is an answer to this question because it has a double-O, a double-K, and a double-E.
     """
     with open("../sowpods.txt", "r") as file:
-        all_triple_double_letter_words = ""
+        all_triple_double_letter_words = []
         for line in file:
             word = line.rstrip("\n")
             pass_triple_double_letter_test = [False, 0]
             double_letter_tracker = dict()
             for letter in word:
                 if letter in double_letter_tracker.keys():
-                    double_letter_tracker[letter] += letter
+                    double_letter_tracker[letter] += [letter]
 
                 else:
-                    double_letter_tracker[letter] = letter
+                    double_letter_tracker[letter] = [letter]
             for key in double_letter_tracker:
                 if len(double_letter_tracker[key]) > 1:
                     pass_triple_double_letter_test[1] += 1
                     if pass_triple_double_letter_test[1] == 3:
-                        all_triple_double_letter_words += f"{word},"
-        print(all_triple_double_letter_words)
+                        all_triple_double_letter_words.append(word)
+        return all_triple_double_letter_words
 
 
 def test_one():
-    assert find_lotsa_double_letters(['BOOKKEEPER']) == 'BOOKKEEPER'
+    assert 'BOOKKEEPER' in find_lotsa_double_letters()
+    
+def test_later_results():
+    assert 'UNGUESSED' in find_lotsa_double_letters() 
     
