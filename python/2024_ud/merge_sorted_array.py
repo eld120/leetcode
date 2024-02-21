982529c760ffe7b7f27f7d64df542f7ce57d86c7
Seyamack Afzali <seyamack@gmail.com>
2024-02-21 14:36:42 -0600

    struggled with the mental model here

---
+++ python/2024_ud/merge_sorted_array.py
@@ -0,0 +1,70 @@
+from typing import List
+
+
+def merge_lists(nums1: List[int], m: int, nums2: List[int], n: int ) -> None:
+    '''
+    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+    Example 1:
+
+    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    Output: [1,2,2,3,5,6]
+    Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+    The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+    Example 2:
+
+    Input: nums1 = [1], m = 1, nums2 = [], n = 0
+    Output: [1]
+    Explanation: The arrays we are merging are [1] and [].
+    The result of the merge is [1].
+    Example 3:
+
+    Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+    Output: [1]
+    Explanation: The arrays we are merging are [] and [1].
+    The result of the merge is [1].
+    Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+    
+    Constraints:
+    nums1.length == m + n
+    nums2.length == n
+    0 <= m, n <= 200
+    1 <= m + n <= 200
+    -109 <= nums1[i], nums2[j] <= 109
+    
+
+    Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+    '''
+    
+    index = 0
+    val_index = 0
+    for val in nums1:
+        
+        if index < n and val > nums2[index]:
+            nums1.insert(val_index, nums2[index])
+            index += 1
+        elif index < n and val_index >= m + index:
+            print(nums1)
+            nums1.insert(val_index, nums2[index])
+            index += 1
+            nums1.pop(-1)
+        elif len(nums1) > n+m:
+            while len(nums1) > n+m:
+                nums1.pop(-1)
+        
+        val_index +=1
+    
+    return nums1
+
+def test_one():
+    assert merge_lists([1,2,3,0,0,0], 3, [2,5,6], 3) == [1,2,2,3,5,6]
+
+def test_two():
+    assert merge_lists([0], 0, [1], 1) == [1]
+
+def test_three():
+    assert merge_lists([4,0,0,0,0,0], 1, [1,2,3,5,6], 5) == [1,2,3,4,5,6]
