13e4258ba53e046037ee654ae888cfa46391cc8d
Seyamack Afzali <seyamack@gmail.com>
2024-03-06 09:28:50 -0600

    really screwed up first time around, fixed now

--- last_algo_course/binary_search.py
+++ last_algo_course/binary_search.py
@@ -9,27 +9,31 @@ def binary_find_stuff(searchable: List, hopefully_findable: int) -> int:
     #     raise ValueError
     while low <= high:
         midpoint = (high + low)//2
-        if hopefully_findable == midpoint:
+        if hopefully_findable == searchable[midpoint]:
             return midpoint
-        elif hopefully_findable < midpoint :
-            high = midpoint
+        elif hopefully_findable < searchable[midpoint] :
+            # does the following need to be midpoint - 1? what edge case does this cover?
+            high = midpoint 
         else:
             low = midpoint + 1
         
 
+def test_lower_bound():
+    assert binary_find_stuff([1,2,3,4,5,6,7], 2) == 1
+
 def test_one():
-    assert binary_find_stuff([num for num in range(1,101)], 23) == 23
-    assert binary_find_stuff([1,2,3], 2) == 2 
+    assert binary_find_stuff([num for num in range(1,101)], 12) == 11
+    assert binary_find_stuff([1,2,3], 2) == 1
 
 def test_two():
-    assert binary_find_stuff([num for num in range(1,101)], 22) == 22
+    assert binary_find_stuff([num for num in range(1,101)], 22) == 21
 
 def test_odd_number():
-    assert binary_find_stuff([1,2,3,4,5,6,7], 7) == 7
+    assert binary_find_stuff([1,2,3,4,5,6,7], 7) == 6
 
 
 def test_three():
-    assert binary_find_stuff([num for num in range(1,1000001)], 954126) == 954126
+    assert binary_find_stuff([num for num in range(1,1000001)], 954126) == 954125
 
 def test_four():
-    assert binary_find_stuff([num for num in range(1,1000001)], 54376) == 54376
+    assert binary_find_stuff([num for num in range(1,1000001)], 54376) == 54375
