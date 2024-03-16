f74613433e6dc8e7a74e04d76035693e773c3e36
Seyamack Afzali <seyamack@gmail.com>
2024-03-15 22:25:21 -0500

    already done, added tests + ll_builder

--- python/2024_ud/remove_duplicates_linked_list.py
+++ python/2024_ud/remove_duplicates_linked_list.py
@@ -24,16 +24,19 @@ The list is guaranteed to be sorted in ascending order.
 Seen this question in a real interview before?
 1/4
 '''
-from typing import Optional
+from typing import Optional, List
 
 
 class ListNode:
-    def __init__(self, val=0, next=None):
+    def __init__(self, val=0, nxt=None):
         self.val = val
-        self.next = next
+        self.next = nxt
     
     def __repr__(self):
         return f'val: {self.val}, next: {self.next}'
+    
+    def __eq__(self, other) -> bool:
+        return self.__repr__() == other.__repr__()
 
 def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
     '''
@@ -55,9 +58,9 @@ def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
         duplicates.add(head.val)
     #breakpoint()
     current = head
-    while current:
+    while current is not None:
         # [1,1,2]
-        if current.next:
+        if current.next is not None:
             if current.next.val not in duplicates:
                 duplicates.add(current.next.val)
                 current = current.next
@@ -65,7 +68,7 @@ def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
                 second = current.next
                 if current.next.next:
                     current.next = second.next
-                    second = None
+                    del second
                 else:
                     current.next = None
         else:
@@ -76,3 +79,24 @@ def deleteDuplicates(head: Optional[ListNode]) -> Optional[ListNode]:
 
 
 
+def build_linked_list(inputlist: List[int]) -> ListNode:
+    ''' builds linked list from a list of ints'''
+    head = ListNode()
+    current = head
+    for index, val in enumerate(inputlist):
+        if index == len(inputlist) -1:
+            current.val = val
+            current.next = None
+        else:
+            current.val = val
+            current.next = ListNode()
+            current = current.next
+    return head
+
+def test_build_ll():
+    assert build_linked_list([1,2,3,4,3]) == ListNode(1, ListNode(2,(ListNode(3, ListNode(4, ListNode(3, None))))))
+
+
+def test_simple():
+    '''delete single duplicate'''
+    assert deleteDuplicates(build_linked_list([1,1,2])) == ListNode(1,ListNode(2, None))
