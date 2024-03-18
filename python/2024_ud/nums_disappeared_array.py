e9028fb3fb5747e044e3b912a8545548c18729a7
Seyamack Afzali <seyamack@gmail.com>
2024-03-18 09:40:08 -0500

    meh

---
+++ python/2024_ud/nums_disappeared_array.py
@@ -0,0 +1,24 @@
+from collections import Counter
+from typing import List
+
+
+def find_disappeared_num(nums: List[int])-> List[int]:
+    nums.sort()
+    num = set(nums)
+    
+    #breakpoint()
+    return [val for val in range(1, len(nums)+1) if val not in num]
+
+
+def find_disappeared(nums: List[int])-> List[int]:
+    n = set()
+    highest = 0
+    for num in  nums:
+        n.add(num)
+        if num >= highest:
+            highest = num
+    return [num for num in range(1, len(nums) +1) if num not in n]
+
+
+def test_one_one():
+    assert find_disappeared_num([1,1]) == [2]
