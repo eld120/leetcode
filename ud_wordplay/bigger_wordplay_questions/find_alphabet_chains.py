671e564656be42fd872459ef8d1df1ed5e2fe4a4
Seyamack Afzali <seyamack@gmail.com>
2024-03-20 14:43:40 -0500

    wrapped up last wordplay w/ Charley

--- ud_wordplay/bigger_wordplay_questions/find_alphabet_chains.py
+++ ud_wordplay/bigger_wordplay_questions/find_alphabet_chains.py
@@ -6,16 +6,16 @@
 
 
 '''
-from typing import List
+from typing import List, Tuple
 
 # function that parses sowpods.txt
 # return a list 
-def parse_sowpods() -> List:
-    with open('../sowpods.txt', 'r') as file:
+def parse_file(file_path: str) -> List: 
+    with open(file_path, 'r') as file: # pylint: disable=W1514
         return [line.rstrip('\n') for line in file]
 
 
-def alphabet_chain(sowpods_list=parse_sowpods()):
+def alphabet_chain(sowpods_list=parse_file('C:/Users/seyam/Sandbox/l33tcode/leetcode/ud_wordplay/sowpods.txt')):
     # create a list of words that contain an alphabet chain
     alphabet_chain_words = []
     # iterate through the list of sowpod words
@@ -83,11 +83,9 @@ def longest_alphabet_chain_in_word(input_string: str):
     longest_alphabet_chain = []
     current_alphabet_chain = []
     #breakpoint()
-    # ['D', 'E', 'I', 'O', 'Q', 'R', 'S', 'T', 'U', 'U']
+    # ['D', 'E', 'I', 'O', 'Q', 'R', 'S', 'T', 'U', 'U'] 
     for char in sorted(set(input_string)):
-        print(f'current_alphabet_chain: {current_alphabet_chain}')
-        print(f'longest_alphabet_chain: {longest_alphabet_chain}')
-        print(f'char: {char}')
+       
         if not current_alphabet_chain:
             current_alphabet_chain.append(char)
         elif ord(current_alphabet_chain[-1]) + 1 == ord(char):
@@ -102,13 +100,53 @@ def longest_alphabet_chain_in_word(input_string: str):
     
 
 
+def find_longest_alphabet_chain_in_file(file_path: str)-> Tuple[List[str], List[str]]:
+    '''
+    given all of the words in sowpods find the longest alphabet chain, words that are m
+    '''
+    # parse sowpods
+    sowpods = parse_file(file_path)
+    # vars: longest_chain , list_of_words (words made from longest alphabet chains)
+    alphabet_chains = dict()
+    longest_chain_length = 0
+    # for every word in sowpods call (longest_alphabet chain)
+    for word in sowpods:
+        #current_alphabet_chain = longest_alphabet chain method
+        current_alphabet_chain = longest_alphabet_chain_in_word(word)
+        hashable_chain = "".join(sorted(current_alphabet_chain))
+        # if len(current_alphabet chain) > len(longest_chain) then current becomes the longest
+        if len(current_alphabet_chain) > longest_chain_length:
+            longest_chain_length = len(current_alphabet_chain)
+            alphabet_chains = dict()
+            alphabet_chains[hashable_chain] = [word]
+
+
+        elif len(current_alphabet_chain) == longest_chain_length:    
+            if hashable_chain not in alphabet_chains.keys():
+                alphabet_chains[hashable_chain] = [word]
+            else:
+                alphabet_chains[hashable_chain] += [word]
+
+
+
+    print(longest_chain_length)
+    return alphabet_chains
+
+    
+
+
+
 
 
 
 def test_feedback():
     assert alphabet_chain(['FEEDBACK', 'NOTAWORD', '', 'FEEDBACKS', 'ZZZ']) == ['FEEDBACK', 'FEEDBACKS']
 
+def test_longest_chain_turquoised():
+    assert longest_alphabet_chain_in_word('TURQUOISED') == ['Q', 'R', 'S', 'T', 'U']
+
+
 
 
 if __name__ == '__main__':
-    print(longest_alphabet_chain_in_word('TURQUOISED'))
+    print(find_longest_alphabet_chain_in_file('C:/Users/seyam/Sandbox/l33tcode/leetcode/ud_wordplay/sowpods.txt'))
