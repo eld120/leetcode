cf185be36e5dd2068fcd58229ef248ee400632df
Seyamack Afzali <seyamack@gmail.com>
2024-03-21 10:38:35 -0500

    trivial for a medium

---
+++ python/2024_ud/top_k_frequent_elements.py
@@ -0,0 +1,8 @@
+from collections import Counter
+from typing import List
+
+
+def top_k_frequent(nums: List[int], k:int)-> List[int]:
+    count = Counter(nums)
+    return [x[0] for x in count.most_common(2)]
+
