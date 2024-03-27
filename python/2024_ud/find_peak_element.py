79bc0c8e2e229a98646095ec9b254e5d9d629670
Seyamack Afzali <seyamack@gmail.com>
2024-03-27 10:24:58 -0500

    more complicated than it should have been

--- python/2024_ud/find_peak_element.py
+++ python/2024_ud/find_peak_element.py
@@ -37,48 +37,15 @@ def find_peak(nums: List[int])-> int:
         return right
     else:
         return midpoint
-    # if left > midpoint:
-    #     if midpoint > right:
-    #         return midpoint
-    #     else:
-    #         return left
-    # elif right > midpoint:
-    #     if right > left:
-    #         return right
-    #     else:
-    #         return left
-    # else:
-    #     return midpoint
-    
-def find_peak_again(something):
-    
-    left = 0
-    right = len(something)
-    if right <= 2:
-        h = -float("inf")
-        top = 0
-        for i, val in enumerate(something):
-            if val > h:
-                top = i
-                h = val
-        return top
-    
-    while left < right:
 
-        mid = (left + right)//2
-        if something[mid] < something[mid+1]:
-            left = mid + 1
-        else:
-            right = mid - 1
 
-    return left
 
 
 def test_one():
-    assert find_peak_again([1,2,1,3,5,6,4]) == 1 or find_peak([1,2,1,3,5,6,4]) == 5
+    assert find_peak([1,2,1,3,5,6,4]) == 1 or find_peak([1,2,1,3,5,6,4]) == 5
 
 def test_36():
-    assert find_peak_again([6,5,4,3,2,3,2]) == 0
+    assert find_peak([6,5,4,3,2,3,2]) == 0
 
 def test_27():
-    assert find_peak_again([1,2,3,1,]) == 2
+    assert find_peak([1,2,3,1,]) == 2
