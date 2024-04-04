9849f3656c8139cdcc8a7388a273d10eff125a9e
Seyamack Afzali <seyamack@gmail.com>
2024-04-03 22:20:02 -0500

    k largest again w/ charley

--- python/2024_ud/k_largest_elements.py
+++ python/2024_ud/k_largest_elements.py
@@ -1,6 +1,6 @@
 from typing import List
 
-
+import heapq
 
 
 def k_largest_elements(array: List[int], k: int) -> List[int]:
@@ -16,7 +16,7 @@ def k_largest_elements(array: List[int], k: int) -> List[int]:
     array.sort()
     return array[-k:]
 
-def k_largets_ele_no_sorting(array: List[int], k: int) -> List[int]:
+def k_largest_ele_no_sorting(array: List[int], k: int) -> List[int]:
     top_nums = []
     for num in array:
         top_nums.append(num)
@@ -38,12 +38,36 @@ class KthLargest:
         return self._array[-self._k]
 
 
+def k_largest_element(array: List[int], k: int) -> List[int]:
+    smallest_nums = []
+    heapq.heapify(smallest_nums)
+    for num in array:
+        heapq.heappush(smallest_nums, num)
+        if len(smallest_nums) > k:
+            heapq.heappop(smallest_nums)
+        
+    return smallest_nums
+        
+
+def k_elements_again(array: List[int], k: int) -> List[int]:
+    
+
+    smallest_nums = array[:k]
+    heapq.heapify(smallest_nums)
+
+    for index in range(k, len(array)):
+        heapq.heappushpop(smallest_nums, array[index])
+    
+    return smallest_nums
+
 
 def test_ints_duplicates():
-    assert k_largets_ele_no_sorting([7,16,5,16,-1,-1,3,11,2],3) == [11,16,16]
+    assert k_elements_again([7,16,5,16,-1,-1,3,11,2],3) == [11,16,16]
 
 def test_empty_array():
-    assert k_largets_ele_no_sorting([], 0) == []
+    assert k_elements_again([], 0) == []
 
 def test_k_larger_than_array():
-    assert k_largets_ele_no_sorting([1,2,3], 4) == [1,2,3]
+    assert k_elements_again([1,2,3], 4) == [1,2,3]
+
+
