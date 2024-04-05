dfca92d855233a9fd693846bec8ca9aa3802be2c
Seyamack Afzali <seyamack@gmail.com>
2024-04-05 16:27:53 -0500

    optimal + less than 5 mins

---
+++ python/2024_ud/reverse_string.py
@@ -0,0 +1,20 @@
+
+
+
+def reverse_string(s: list[str]) -> None:
+
+    left= 0
+    right = len(s)-1
+    while left< right:
+        temp_l = s[left]
+        temp_r = s[right]
+        s[left] = temp_r
+        s[right] = temp_l
+        left += 1
+        right -= 1
+    return s
+    
+def test_one():
+    assert reverse_string(["h","e","l","l","o"]) == ["o","l","l","e","h"]
+
+    
