6676d958c7efc9980d4b2c64e7fba27b6dd1be7a
Seyamack Afzali <seyamack@gmail.com>
2024-04-06 22:33:54 -0500

    annoying but w/e

--- python/2024_ud/search_2d_matrix.py
+++ python/2024_ud/search_2d_matrix.py
@@ -60,9 +60,52 @@ def searchMatrix(matrix: List[List[int]], target: int) -> bool:
     return False
 
 
+def search_matrix_recursive(matrix, target):
+    
+    lower = 0
+    upper = len(matrix) -1
+    #breakpoint()
+    def recurse(matrix, lower, upper, target):
+        #breakpoint()
+        midpoint = (lower+upper) // 2
+        if matrix[midpoint][0] <= target and target <= matrix[midpoint][-1]:
+            left = 0
+            right = len(matrix[midpoint]) -1
+            
+            while left < right:
+                mid = (left + right) // 2
+                if matrix[midpoint][mid] == target:
+                    return True
+                elif matrix[midpoint][mid] > target:
+                    right = mid -1
+                else:
+                    left = mid + 1
+            return True if matrix[midpoint][(left + right) // 2] == target else False
+        elif matrix[midpoint][-1] < target:
+            lower = midpoint + 1
+        else:
+            upper = midpoint - 1
+        if lower > upper:
+            return False if target not in matrix[midpoint] else True
+        return recurse(matrix, lower, upper, target)
+        
+    return recurse(matrix, lower, upper, target)
+
+
 def test_one():
-    assert searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3) == True
+    assert search_matrix_recursive([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3) == True
 
 def test_two():
-    assert searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13) == False
-         
+    assert search_matrix_recursive([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13) == False
+
+def test_three():
+    assert search_matrix_recursive([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 5) == True
+
+def test_len_two_false():
+    assert search_matrix_recursive([[1], [3]], 0) == False
+
+def test_len_two_true():
+    assert search_matrix_recursive([[1], [3]], 1) == True
+
+def test_tired():
+    assert search_matrix_recursive([[1],[3],[5]], 3) == True
