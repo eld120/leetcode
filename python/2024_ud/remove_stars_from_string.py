7c456fc035a32b0f2b6a6196a583d95243c17690
Seyamack Afzali <seyamack@gmail.com>
2024-04-09 23:32:11 -0500

    took 5 mins, pretty easy

---
+++ python/2024_ud/remove_stars_from_string.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+
+def remove_stars(s: str) -> str:
+    stack = deque()
+    for char in s:
+
+        if char == '*':
+            if len(stack) > 0:
+                stack.pop()
+        else:
+            stack.append(char)
+    
+    return "".join(stack)
+
+
+def test_one():
+    assert remove_stars("leet**cod*e") == 'lecoe'
+
+def test_two():
+    assert remove_stars("erase*****") == ''
