21961ebc6673800206e0afb04c109bf09b5fe4c8
Seyamack Afzali <seyamack@gmail.com>
2024-04-17 22:17:45 -0500

    another great refactor session w/ Charley

--- python/2024_ud/connect_four.py
+++ python/2024_ud/connect_four.py
@@ -16,6 +16,70 @@ def check_vertical_win(board: list[list[int]], last_move: tuple[int]) -> bool:
 
     return connect_four
 
+def check_diagonal_win_positive_slope(board: list[list[int]], last_move: tuple[int]) -> bool: 
+    # count_matches, row, col = last_move
+    count_matches = 0
+    row, col = last_move
+    last_move_color = board[row][col]
+    # iterate up/right until diagonal no longer matches or outside of matrix
+    # for _ in range(3)
+    while row >= 0  and col < len(board[0]) and board[row][col] == last_move_color:
+        count_matches += 1
+        row -= 1
+        col += 1
+        
+    row, col = last_move
+    row += 1
+    col -= 1
+    while row < len(board) and col > 0 and board[row][col] == last_move_color:
+        count_matches += 1
+        row += 1
+        col -= 1
+    
+    return count_matches >= 4
+    
+
+    # iterate down/left until diagonal no longer matches or outside of matrix
+    # given the position and value of our last move, check up + right also check down + left
+
+    # checking diagonal positive slope is current index = (row, col), next = (row + 1, col -1) or (row - 1, col + 1)
+    
+    # traverse matrix, ensure up/right or down/left is within matrix ~ if row in range(board) and col in range(board[0])
+
+   
+
+
+
+
+
+
+
+
+board_diagonal_win_positive_slope = [
+    [None , None, None, None, None, None,  1  ],
+    [None , None, None, None, None,  1  , None],
+    [None , None, None, None,   1,  None, None],
+    [None , None, None,   1,  None, None, None],
+    [None , None, None, None, None, None, None],
+    [None , None, None, None, None, None, None],
+]
+
+board_diagonal_loss_positive_slope = [
+    [None , None, None, None, None, None,  1  ],
+    [None , None, None, None, None,  1  , None],
+    [None , None, None, None,   0,  None, None],
+    [None , None, None,   1,  None, None, None],
+    [None , None, 1, None, None, None, None],
+    [None , 1, None, None, None, None, None],
+]
+board_diagonal_loss_positive_slope_three_in_row = [
+    [None , None, None, None, None, None,  None  ],
+    [None , None, None, None, None,  None  , None],
+    [None , None, None, None, None,  None, None],
+    [None , None, 1,  None,  None, None, None],
+    [None , 1, None, None, None, None, None],
+    [1 , None, None, None, None, None, None],
+]
 
 
 board_vertical_win = [
@@ -58,3 +122,13 @@ def test_vertical_loss_too_few():
 
 def test_vertical_loss_non_contiguous():
     assert check_vertical_win(board_vertical_loss_non_contiguous, (1,0)) == False
+
+
+def test_diagonal_win_positive_slope():
+    assert check_diagonal_win_positive_slope(board_diagonal_win_positive_slope, (1,5)) == True
+
+def test_diagonal_loss_positive_slope():
+    assert check_diagonal_win_positive_slope(board_diagonal_loss_positive_slope, (4,2)) == False
+
+def test_diagonal_loss_three_values():
+    assert check_diagonal_win_positive_slope(board_diagonal_loss_positive_slope_three_in_row, (4,1)) == False
