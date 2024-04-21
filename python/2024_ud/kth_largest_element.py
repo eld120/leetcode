c2c2d0a14b3f71e5fc06de77e8e189f047ffb80d
Seyamack Afzali <seyamack@gmail.com>
2024-04-20 22:10:10 -0500

    trivial when using heapq

---
+++ python/2024_ud/kth_largest_element.py
@@ -0,0 +1,14 @@
+import heapq
+
+
+def find_kth_largest_element(nums: list[int], k) -> int:
+    heapq.heapify(nums)
+    largest = heapq.nlargest(k, nums)
+    return largest[-1]
+
+
+def test_one():
+    assert find_kth_largest_element([3,2,1,5,6,4],  2) == 5
+
+def test_two():
+    assert find_kth_largest_element([3,2,3,1,2,4,5,5,6],  4) == 4
