ffc8170c8d53a6aff3b1a71deec51d70f01d2dfa
Seyamack Afzali <seyamack@gmail.com>
2024-04-24 22:37:21 -0500

    basically the same solution as before

---
+++ python/2024_ud/repeats/len_last_word.py
@@ -0,0 +1,26 @@
+'''
+Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+A word is a maximal 
+substring
+ consisting of non-space characters only.
+'''
+import string
+def length_last_thing(s: str) -> int:
+    counter = 0
+    chars = set(string.ascii_letters)
+    for char in reversed(s):
+        if char in chars:
+            counter += 1
+        elif char == " " and counter == 0:
+            continue
+        else:
+            break
+    return counter
+
+
+def test_one():
+    assert length_last_thing('Hello World') == 5
+
+def test_two():
+    assert length_last_thing(' fly me to the moon ') == 4
