3d4313ed2e12fa4a4b924ae2ad75ae4ee02df0bc
Seyamack Afzali <seyamack@gmail.com>
2024-05-07 10:21:31 -0500

    tiny changes huge boost in speed

--- python/2024_ud/reorder_linked_list.py
+++ python/2024_ud/reorder_linked_list.py
@@ -30,7 +30,30 @@ def reorder_list(head: ListNode):
     return head
 
 
-def test_one():
-    assert reorder_list(ll_builder([1,2,3,4])) == ll_builder([1,4,2,3])
+def reorder_faster(head: ListNode):
+    nodes = []
+    current = head
+    while current:
+        nodes.append(current)
+        current = current.next
+    forward_pointer = 1
+    reverse_pointer = len(nodes) - 1
+    index = 0
+    current = head
+    for _ in nodes:
+        if index == len(nodes) - 1:
+            current.next = None
+        elif index % 2 == 0:
+            current.next = nodes[reverse_pointer]
+            reverse_pointer -= 1
+        else:
+            current.next = nodes[forward_pointer]
+            forward_pointer += 1
+        current = current.next
+        index += 1
+    return head
 
-test_one()
+
+def test_one():
+    assert reorder_faster(ll_builder([1,2,3,4])) == ll_builder([1,4,2,3])
+
