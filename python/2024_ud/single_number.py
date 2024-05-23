fabf172e615ef7cc18e37748611fac89a9ec6fee
Seyamack Afzali <seyamack@gmail.com>
2024-05-23 09:59:37 -0500

    forgot to hit save again

--- python/2024_ud/single_number.py
+++ python/2024_ud/single_number.py
@@ -1,4 +1,4 @@
-
+from collections import Counter
 
 def single_num(nums: list[int]) -> int:
     tracker = set()
@@ -8,6 +8,13 @@ def single_num(nums: list[int]) -> int:
         else:
             tracker.remove(val)
     return tracker.pop()
+
+def single_number_On_space(nums):
+    tracker = Counter(nums)
+    for val, count in tracker.items():
+        if count == 1:
+            return val
+
 
 def test_one():
     assert single_num([3,3,2,3]) == 2
