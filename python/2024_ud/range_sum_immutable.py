5a7a273072cc494c9254ce32cb33ac27e0eeb9f8
Seyamack Afzali <seyamack@gmail.com>
2024-06-18 14:02:49 -0500

    apparently this was done on May 25 but untested

--- python/2024_ud/range_sum_immutable.py
+++ python/2024_ud/range_sum_immutable.py
@@ -13,6 +13,7 @@ class NumArray:
     def sumRange(self, left: int, right: int) -> int:
         while left <= right:
             self.answer += self.nums[left]
+            
             left+= 1
         temp = self.answer
         self.answer = 0
@@ -21,3 +22,10 @@ class NumArray:
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
+
+def test_example_one():
+    obj = NumArray([-2, 0, 3, -5, 2, -1])
+    test_input = [[0, 2], [2, 5], [0, 5]]
+    test_output = [1, -1, -3]
+    for index, val in enumerate(test_input):
+        assert obj.sumRange(val[0], val[1]) == test_output[index]
