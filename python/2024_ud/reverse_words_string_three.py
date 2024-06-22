ac60df5672f93ce3383711366c1abc4f56951f68
Seyamack Afzali <seyamack@gmail.com>
2024-06-21 23:03:16 -0500

    five mins each

--- python/2024_ud/reverse_words_string_three.py
+++ python/2024_ud/reverse_words_string_three.py
@@ -2,5 +2,4 @@
 
 
 def rever_words(s: str) -> str:
-    arr = s.split()
-    return "".join([f"{word[::-1]} " for word in arr]).strip()
+    return "".join([f"{word[::-1]} " for word in s.split()]).strip()
